990f9534e56798ffeb02db9bf549ed95dfc69291
D0P3ST <43076785+D0P3ST@users.noreply.github.com>
2018-09-14 14:34:21 -0600

    Add files via upload

--- CS 271 project1/sprint1/test/LoginPanelBoardTest.java
+++ CS 271 project1/sprint1/test/LoginPanelBoardTest.java
@@ -17,13 +17,13 @@ import sprint1.UserDatabase;
  * @author dillionrichey
  *
  */
-public class LoginPanelBoardTest { 
-	
+public class LoginPanelBoardTest {
+
 	public LoginPanelBoard login = new LoginPanelBoard();
 
 	int i = 0;
 	int j = 0;
-	
+
 	@Before
 	public void setUp() throws Exception {
 	}
@@ -37,61 +37,100 @@ public class LoginPanelBoardTest {
 	 */
 	@Test
 	public void testEmailExists() {
-		
-	
+
 		UserDatabase db;
-		
+
 		try {
 			db = new UserDatabase();
-			//db.registerUser("usertest", "Password1", "usernameEmail@email.com", null);
-			assertTrue("", db.emailExists("usernameEmail@email.com")==true); 
-		}
-		 catch (IOException ex) {
+			// db.registerUser("usertest", "Password1",
+			// "usernameEmail@email.com", null);
+			assertTrue("", db.emailExists("usernameEmail@email.com") == true);
+		} catch (IOException ex) {
 			Logger.getLogger(LoginPanelBoard.class.getName()).log(Level.SEVERE, null, ex);
 		}
-		
-	
-	
+
 	}
-	
+
 	/**
 	 * tests if username is input properly
 	 */
 	@Test
 	public void testUsername() {
-		
+
 		UserDatabase db;
-		
+
 		try {
-			 db = new UserDatabase();
+			db = new UserDatabase();
 			db.logIn("usertest", "Password1");
-			assertTrue("", db.userExists("usertest")==true);
-		}
-		catch (IOException ex) {
+			assertTrue("", db.userExists("usertest") == true);
+		} catch (IOException ex) {
 			Logger.getLogger(LoginPanelBoard.class.getName()).log(Level.SEVERE, null, ex);
 		}
-		
-		
+
 	}
-	
+
 	/**
 	 * test login is successful
 	 */
 	@Test
 	public void testlogin() {
-		
+
 		UserDatabase db;
-		
+
 		try {
-			 db = new UserDatabase();
-			 db.logIn("usertest", "Password1");
+			db = new UserDatabase();
+			db.logIn("usertest", "Password1");
 			db.getLoginState();
-			assertTrue(db.getLoginState()==true);
-		}
-		catch (IOException ex) {
+			assertTrue(db.getLoginState() == true);
+		} catch (IOException ex) {
 			Logger.getLogger(LoginPanelBoard.class.getName()).log(Level.SEVERE, null, ex);
 		}
 
 	}
-	
+
+	/*
+	 * Test registering a user to database
+	 * 
+	 */
+	@Test
+	public void testRegisterUser() {
+		UserDatabase db;
+
+		try {
+			db = new UserDatabase();
+			db.registerUser("usertest1", "Password1!", "usernameEmail@email.com", null);
+			db.logIn("usertest1", "Password1!");
+			assertTrue(db.getLoginState() == true);
+			db.deleteUser("username1");
+		} catch (IOException ex) {
+			Logger.getLogger(LoginPanelBoard.class.getName()).log(Level.SEVERE, null, ex);
+		}
+
+	}
+
+	/*
+	 * 
+	 * Test to delete users from the database
+	 * 
+	 * 
+	 */
+	@Test
+	public void testDeleteUser() {
+		UserDatabase db;
+		try {
+			db = new UserDatabase();
+			db.registerUser("usertest1", "Password1!", "usernameEmail@email.com", null);
+			db.logIn("usertest1", "Password1!"); // create user
+			if (db.getLoginState() == true) { // if user successfully created
+				db.deleteUser("usertest1"); // delete user
+				assertTrue(db.getLoginState() == false); // Check to make sure
+															// registered
+															// user.equals false
+			} else if (db.getLoginState() == false) {
+				System.out.print("Failed successfully login!");
+			}
+		} catch (IOException ex) {
+			Logger.getLogger(LoginPanelBoard.class.getName()).log(Level.SEVERE, null, ex);
+		}
+	}
 }
